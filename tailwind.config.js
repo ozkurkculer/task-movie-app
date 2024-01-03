b6a1ee9b7af12a770bbb47966dc4bc9698e3418c
Mehmet Emin Özkürkçüler <boraa.durna@gmail.com>
2024-01-03 21:01:30 +0300

    fix: change content directions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     colors: {
       'primary': '#0296E5',
-      'secondary': '#3A3F47',
+      'secondary': '#67686D',
       'third': '#3A3F47',
-    },
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    }
   },
   plugins: [],
 }
