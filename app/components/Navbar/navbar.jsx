68645a46d1fa1e0a29a0765af96846c8ce8235d5
Mehmet Emin Özkürkçüler <boraa.durna@gmail.com>
2024-01-03 21:05:51 +0300

    fix: remove some props and edit classes

--- app/components/Navbar/navbar.jsx
+++ app/components/Navbar/navbar.jsx
@@ -1,12 +1,11 @@
-import React from "react";
 import NavbarItem from "../NavbarItem";
 
 function Navbar() {
   return (
-    <div className="w-full bg-third flex flex-row justify-between px-10 py-4">
-      <NavbarItem name={'Home'} icon={'/icons/Home.svg'}/>
-      <NavbarItem name={'Search'} icon={'/icons/Search.svg'}/>
-      <NavbarItem name={'Watch list'} icon={'/icons/Save.svg'}/>
+    <div className="fixed bottom-0 w-full border-t-2 border-primary bg-third flex flex-row justify-between px-10 py-4 ">
+      <NavbarItem name={'Home'} direction={''} svgname={'HomeIcon'} />
+      <NavbarItem name={'Search'} direction={'search'} svgname={'SearchIcon'}/>
+      <NavbarItem name={'Watch list'} direction={'watchlist'} svgname={'SaveIcon'}/>
     </div>
   )
 }
