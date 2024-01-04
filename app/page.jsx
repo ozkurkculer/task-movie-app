d1527e9fbbd64900f5a9aa1485d31a08c80b3193
Mehmet Emin Özkürkçüler <boraa.durna@gmail.com>
2024-01-04 22:25:59 +0300

    feat: add sections and fetching functions

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,77 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import MovieService from "./api/movie.service"
+import ImagePoster from "./components/ImagePoster"
+import Tabs from "./components/Tabs"
+
 export default function Home() {
+  const [popularMoviePosterPaths, setPopularMoviePosterPaths] = useState([]);
+  const [nowPlayingMoviePosterPaths, setNowPlayingMoviePosterPaths] = useState([]);
+  const [topRatedMoviePosterPaths, setTopRatedMoviePosterPaths] = useState([]);
+
+  useEffect(() => {
+    fetchPopularMovies();
+    fetchTopRatedMovies();
+    fetchNowPlayingMovies();
+  }, [])
+
+  const fetchPopularMovies = async () => {
+    const paths = await MovieService.getPopularMovies();
+    setPopularMoviePosterPaths(paths);
+  };
+
+  const fetchTopRatedMovies = async () => {
+    const paths = await MovieService.getTopRatedMovies();
+    setTopRatedMoviePosterPaths(paths);
+  };
+
+  const fetchNowPlayingMovies = async () => {
+    const paths = await MovieService.getNowPlayingMovies();
+    setNowPlayingMoviePosterPaths(paths);
+  };
+
+  const topRatedPosters = topRatedMoviePosterPaths.map((path, index) => (
+    <ImagePoster path={path} key={index} num={index + 1} classes="h-auto w-[145px] sm:w-[145px] md:w-[170px] lg:w-[200px] xl:w-[225px] rounded-2xl" mainClasses="h-auto w-[175px] sm:w-[175px] md:w-[200px] lg:w-[230px] xl:w-[255px]" />
+  ));
+
+  const popularPosters = popularMoviePosterPaths.map((path, index) => (
+    <ImagePoster path={path} key={index} classes="h-auto w-[200px] sm:w-[90px] md:w-[100px] rounded-2xl" />
+  ));
+
+  const nowPlayingPosters = nowPlayingMoviePosterPaths.map((path, index) => (
+    <ImagePoster path={path} key={index} classes="h-auto w-[200px] sm:w-[90px] md:w-[100px] rounded-2xl" />
+  ));
+
+  const tabs = [
+    { id: 'nowplaying', label: 'Now Playing', content: <>{nowPlayingPosters}</> },
+    { id: 'tab2', label: 'Tab 2', content: <p>Content for Tab 2</p> },
+    { id: 'popular', label: 'Popular', content: <>{popularPosters}</> },
+    { id: 'popular', label: 'Popular', content: <>{popularPosters}</> },
+  ];
+
+
   return (
-      <div className='w-fit h-full'>
-        <div className="text-white ">A</div>
+    <div className='w-full h-full pt-12 px-6'>
+      <h1 className="text-white font-bold text-3xl">What do you want to watch?</h1>
+      <div className="w-full flex items-center justify-between h-10 px-6 text-secondary bg-[#3A3F47] rounded-xl">
+        <p>Search</p>
+        <svg width="22" height="24" viewBox="0 0 22 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g id="Search">
+            <g id="Search_2">
+              <ellipse id="Ellipse_739" cx="10.7885" cy="11.7666" rx="8.14181" ry="8.98856" stroke="#67686D" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              <path id="Line_181" d="M16.4513 18.4851L19.6433 22" stroke="#67686D" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            </g>
+          </g>
+        </svg>
       </div>
+      <div className="flex flex-row gap-3 flex-nowrap overflow-x-auto no-scrollbar my-5">
+        {topRatedPosters}
+      </div>
+      <section>
+        <Tabs tabs={tabs} />
+        {/* Add content for each tab here */}
+      </section>
+    </div>
   )
 }
