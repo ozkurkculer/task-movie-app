a136ca067a01eba2c6ed76eb690983533e5095b7
Mehmet Emin Özkürkçüler <mehmetemin.ozkurkculer@gmail.com>
2024-01-05 16:26:05 +0300

    fix: edit top rated movies responsivity

--- app/components/ImagePoster/imageposter.jsx
+++ app/components/ImagePoster/imageposter.jsx
@@ -2,7 +2,7 @@ function ImagePoster({ path, num, classes, mainClasses }) {
   return (
     <div className={`relative ${mainClasses}`}>
       <img src={'https://image.tmdb.org/t/p/w500' + path} alt={`Movie Poster`} className={classes} />
-      <p className="absolute bottom-0 left-0 text-7xl font-black poster-text z-10">{num}</p>
+      <p className="absolute pl-3 -bottom-5 -left-3 text-7xl font-black poster-text">{num}</p>
     </div>
   )
 }
