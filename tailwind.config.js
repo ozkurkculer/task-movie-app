e573f922f26d9a9871378eb6fe44fbabc99b78c4
Mehmet Emin Özkürkçüler <mehmetemin.ozkurkculer@gmail.com>
2024-01-05 16:22:36 +0300

    chore: add new color and hide scroll classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,50 @@
+const plugin = require('tailwindcss/plugin')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     colors: {
-      'primary': '#0296E5',
-      'secondary': '#67686D',
-      'third': '#3A3F47',
-      'background': '#242A32',
-      'white': '#fff',
-    }
+      primary: "#0296E5",
+      secondary: "#67686D",
+      third: "#3A3F47",
+      background: "#242A32",
+      white: "#fff",
+      grey: "#92929D",
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".scrollbar-hide": {
+          /* IE and Edge */
+          "-ms-overflow-style": "none",
+
+          /* Firefox */
+          "scrollbar-width": "none",
+
+          /* Safari and Chrome */
+          "&::-webkit-scrollbar": {
+            display: "none",
+          },
+        },
+
+        ".scrollbar-default": {
+          /* IE and Edge */
+          "-ms-overflow-style": "auto",
+
+          /* Firefox */
+          "scrollbar-width": "auto",
+
+          /* Safari and Chrome */
+          "&::-webkit-scrollbar": {
+            display: "block",
+          },
+        },
+      });
+    }),
+  ],
+};
