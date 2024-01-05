7b8fa7b041ca278ed3488a84f73f1199bdd63832
Mehmet Emin Özkürkçüler <mehmetemin.ozkurkculer@gmail.com>
2024-01-05 20:59:57 +0300

    feat: change search and watchlist pages

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+import icons from '../lib/icons'
 
 function Search() {
   return (
-    <>
-    <div>Search</div>
-    </>
+    <div className='w-screen h-screen flex flex-col items-center justify-center gap-4 px-4 text-center'> 
+    <div className="">{icons.error}</div>
+    <h1 className='text-white text-xl font-medium sm:text-4xl sm:font-bold'>404</h1>
+    <h1 className='text-grey text-sm font-light sm:text-lg sm:font-normal'>Sorry, We can't do this right now.</h1>
+    </div>
   )
 }
 
--- app/watchlist/page.jsx
+++ app/watchlist/page.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+import icons from '../lib/icons'
 
 function WatchList() {
   return (
-    <>
-    <div>WatchList</div>
-    </>
+    <div className='w-screen h-screen flex flex-col items-center justify-center gap-4 px-4 text-center'> 
+    <div className="">{icons.magicBox}</div>
+    <h1 className='text-white text-xl font-medium sm:text-4xl sm:font-bold'>There Is No Movie Yet!</h1>
+    <h1 className='text-grey text-sm font-light sm:text-lg sm:font-normal'>Find your movie by Type title, categories, years, etc </h1>
+    </div>
   )
 }
 
