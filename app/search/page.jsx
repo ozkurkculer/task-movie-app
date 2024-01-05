f0c4fa76a0f22e4aca022e77dc87794d529da951
Mehmet Emin Özkürkçüler <mehmetemin.ozkurkculer@gmail.com>
2024-01-05 22:32:21 +0300

    fix: change "cant" usage inside html

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -6,7 +6,7 @@ function Search() {
     <div className='w-screen h-screen flex flex-col items-center justify-center gap-4 px-4 text-center'> 
     <div className="">{icons.error}</div>
     <h1 className='text-white text-xl font-medium sm:text-4xl sm:font-bold'>404</h1>
-    <h1 className='text-grey text-sm font-light sm:text-lg sm:font-normal'>Sorry, We can't do this right now.</h1>
+    <h1 className='text-grey text-sm font-light sm:text-lg sm:font-normal'>Sorry, We can&apos;t do this right now.</h1>
     </div>
   )
 }
